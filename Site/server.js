f5772cfaaf13e53812a62434a55f1054abadae29
eriko <mestrinari@gmail.com>
2023-07-20 11:11:25 -0300

    1.0

--- Site/server.js
+++ Site/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 const app = express()
 
 app.use(bodyParser.json())
 const api = require('./rotas/')
 app.use('/api', api)
-const PORT = 3000
+const PORT = process.env. 
 
 app.get('/', (req, res)=>{
     res.json({
